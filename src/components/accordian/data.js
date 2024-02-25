984d03a82de38da92d6c9597968a9dcfcd41d682
Pavel Ozernov <pavel.ozernov@gmail.com>
2024-02-25 17:10:42 +0600

    adds multi and single selection accrodian

---
+++ src/components/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      "id": 1,
+      "question": "What is React?",
+      "answer": "React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called 'components'."
+    },
+    {
+      "id": 2,
+      "question": "How do you create a component in React?",
+      "answer": "Components in React can be created either by using class syntax or function syntax. Class components require extending the React.Component class, while functional components are simpler and can be created with just a function that returns JSX."
+    },
+    {
+      "id": 3,
+      "question": "What is JSX?",
+      "answer": "JSX is a syntax extension for JavaScript, recommended by React for describing the UI structure. It looks similar to HTML but allows developers to include JavaScript expressions directly inside the markup."
+    },
+    {
+      "id": 4,
+      "question": "What are props in React?",
+      "answer": "Props (short for properties) are a way of passing data from parent to child components, enabling the child to be dynamic and reusable. They are read-only and help to maintain a unidirectional data flow."
+    },
+    {
+      "id": 5,
+      "question": "What is state in React?",
+      "answer": "State is a built-in React object used to contain data or information about the component. A component's state can change over time, and when it does, the component re-renders to reflect the changes."
+    }
+  ]
+
+  export default data
